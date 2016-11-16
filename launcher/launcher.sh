244b5ce707db6da89fb7b429f7e21c981f95ddfb
shashi ranjan <shashiranjan84@gmail.com>
2016-11-15 17:21:26 -0800

    Update launcher.sh

--- launcher/launcher.sh
+++ launcher/launcher.sh
@@ -2,51 +2,51 @@
 
 
 set -o errexit
-set -o pipefail
-set -o nounset
 # set -o xtrace
 
-##############################################################
-#                      Parse Arguments                       #
-##############################################################
+LAUNCHER_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+AWS_REGION="`echo \"$LAUNCHER_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
 
 function usage {
 cat << EOF
-usage: $0 options
 
-This script requests AWS to generate a new key-pair 
+Please set all required environment variables before running the script  
 
-OPTIONS:
- -r      AWS Region
- -n      Key name
+AWS_ACCESS_KEY_ID ( required )
+AWS_SECRET_ACCESS_KEY ( required )
+EBS_VOLUME_SIZE ( optional )
+INATANCE_TYPE ( optional )
+MINION_CLUSTER_SIZE ( optional )
+MASTER_CLUSTER_SIZE ( optional )
+KAY_NAME ( required )
+VPC_AVAILABILITY_ZONES ( optional )
+ALLOW_SSH_FROM ( optional )
+CERT_ARN ( required )
+
 EOF
 }
 
-AWS_REGION=us-east-1
-KEY_NAME=
-while getopts "hr:n:" OPTION
-do
-  case $OPTION in
-    h)
-      usage
-      exit 1
-      ;;
-    r)
-      KONG_VERSION=$OPTARG
-      ;;
-    n)
-      KEY_NAME=$OPTARG
-      ;;
-    ?)
-      usage
-      exit
-      ;;
-  esac
-done
-
-if [[ -z $AWS_REGION ]] || [[ -z $KEY_NAME ]]; then
+if [[ -z $AWS_REGION ]] || [[ -z $KEY_NAME ]] || [[ -z $AWS_ACCESS_KEY_ID ]] || [[ -z $AWS_SECRET_ACCESS_KEY ]] || [[ -z $CERT_ARN ]]; then
   usage
   exit 1
 fi
 
-aws --region $AWS_REGION ec2 create-key-pair --key-name $KEY_NAME | jq -r ".KeyMaterial" > ~/.ssh/$KEY_NAME.pem
+
+##############################################################
+#                      create key-pair                       #
+##############################################################
+
+bash generate_key.sh -r $AWS_REGION -n $KEY_NAME
+
+##############################################################
+#                      stack up cluster                      #
+##############################################################
+# load template parameter
+# ansible-playbook aws_create_swarm_cluster.yml --extra-vars "cf_stack_name=mashape-enterprise-swarm"
+# get elb_dns
+# export DOCKER_HOST=tcp://{{ elb_dns }}:4000
+
+##############################################################
+#                      deploy all apps                       #
+##############################################################
+# bash deployer.sh
